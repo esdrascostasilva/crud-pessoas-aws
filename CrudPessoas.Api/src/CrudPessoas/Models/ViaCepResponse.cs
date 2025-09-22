de48c0e4769089e534103aeffc44238faf3eac52
esdrascosta <esdras.cts@outlook.com>
2025-09-22 12:20:44 -0300

    refactor: adicionando um model pra ler o viacep

---
+++ CrudPessoas.Api/src/CrudPessoas/Models/ViaCepResponse.cs
@@ -0,0 +1,10 @@
+namespace CrudPessoas.Models
+{
+    class ViaCepResponse
+    {
+        public string Logradouro { get; set; } = string.Empty;
+        public string Bairro { get; set; } = string.Empty;
+        public string Localidade { get; set; } = string.Empty;
+        public string Uf { get; set; } = string.Empty;
+    }
+}
